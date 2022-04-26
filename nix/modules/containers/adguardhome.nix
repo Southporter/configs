6873c16c2966ba321285f68dea7eeedc59bbc042
Shem Sedrick <ssedrick1@gmail.com>
2022-04-26 10:33:26 -0500

    Fixing port definition

--- nix/modules/containers/adguardhome.nix
+++ nix/modules/containers/adguardhome.nix
@@ -1,8 +1,8 @@
 { config, pkgs, ... }:
 
 {
-  networking.firewall.allowedTCPPorts = networking.firewall.allowedTCPPorts ++ [ 53 3000 ];
-  networking.firewall.allowedUDPPorts = networking.firewall.allowedUDPPorts ++ [ 53 ];
+  networking.firewall.allowedTCPPorts = [ 53 3000 ];
+  networking.firewall.allowedUDPPorts = [ 53 ];
   virtualization.oci-containers.containers.adguardhome = {
     image = "docker.io/library/adguard/adguardhome";
     ports = [
@@ -10,6 +10,5 @@
       "53:53/tcp"
       "3000:3000/tcp"
     ];
-
   };
 }
