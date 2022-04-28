f7b7d8f50d8985de1e1944ab3634ad469937bc35
Shem Sedrick <ssedrick1@gmail.com>
2022-04-28 09:22:01 -0500

    Configuring gitea

--- nix/gitea.nix
+++ nix/gitea.nix
@@ -7,17 +7,6 @@
     domain = "git.sedrick.lan";
     rootUrl = "http://git.sedrick.lan/";
     httpPort = 3001;
-    extraConfig = let
-      docutils = pkgs.python39.withPackages (ps: with ps; [
-        docutils
-        pygments
-      ]);
-    in ''
-      [markup.restructuredtext]
-      ENABLED = true
-      FILE_EXTENSIONS = .rst
-      RENDER_COMMAND = ${docutils}/bin/rst2html.py
-      IS_INPUT_FILE = false
-    '';
+    useWizard = true;
   };
 }
